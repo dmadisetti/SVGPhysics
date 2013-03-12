ea482c489a76aec20dba4265a024054a24dfb22b
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-03-11 20:55:35 -0400

    DB Fix

--- static/engine.js
+++ static/engine.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
 function start() {
 	number = 0;
 	$.each($('#vectors').children(),function(){
-		if (this.attr("data-type") == 'main')
+		if ($(this).attr("data-type") == 'main')
 			main = number;
 		Objects[number] = new Object();
 		Objects[number].dom = this;
