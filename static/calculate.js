c0948858319ff9ee8b506a08923a81e4ab5413c5
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-11-16 22:07:17 -0500

    Woop woop

--- static/calculate.js
+++ static/calculate.js
@@ -4,7 +4,7 @@ var calculate = {
 			dx = ob.cx()-ob1.cx();
 			dy = ob.cy()-ob1.cy();
 			// No radius?
-			offset = ob1.r - Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
+			offset = ob.r + ob1.r - Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
 			if(offset>0) resolve(ob,ob1,{'bound':{'angle':Math.atan(dy/dx)},'offset':-offset});
 		},
 		'rect': function(ob,ob1){
